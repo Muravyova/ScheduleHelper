43c182f4640000522f967739a087b3c3cdac663b
kiseme <semenyukkirill@gmail.com>
2019-01-20 05:28:04 +0700

    Созданы всплываюшие окна для формы регистрации и создания преподавателя.  Изменён главный экран, удалён футер.

--- Test2App/wwwroot/js/modal.js
+++ Test2App/wwwroot/js/modal.js
@@ -1,12 +1,15 @@
-﻿$(document).ready(function () {
-    //Скрыть PopUp при загрузке страницы
-    PopUpHide();
+﻿$('#open__btn').click(function () {
+    $("#global-wrapper, #container").fadeIn();
+    //$('#modal-wrapper').fadeIn();
+    document.getElementById('global-wrapper').style.filter = 'blur(5px)';
+    TweenMax.from("#container", .4, { scale: 0, ease: Sine.easeInOut });
+    TweenMax.to("#container", .4, { scale: 1, ease: Sine.easeInOut });
 });
-//Функция отображения PopUp
-function PopUpShow() {
-    $("#popup1").show();
-}
-//Функция скрытия PopUp
-function PopUpHide() {
-    $("#popup1").hide();
-}
+
+$(".login__btn--close, .btn-danger").click(function () {
+    TweenMax.from("#container", .4, { scale: 1, ease: Sine.easeInOut });
+    TweenMax.to("#container", .4, { left: "0px", scale: 0, ease: Sine.easeInOut });
+    $("#container").fadeOut(800);
+    //$('#modal-wrapper').fadeOut(800);
+    document.getElementById('global-wrapper').style.filter = 'none';
+});
