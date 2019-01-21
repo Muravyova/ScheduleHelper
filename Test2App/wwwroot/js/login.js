2902eb4d221a464968f74334c3da1938d75aa2fa
VarHoro <varhoro@mail.ru>
2019-01-21 15:11:56 +0700

    'LogInTry'

--- Test2App/wwwroot/js/login.js
+++ Test2App/wwwroot/js/login.js
@@ -1,18 +1,8 @@
-﻿$('#login__btn').click(function () {
-    $('#login__btn').fadeOut("slow", function () {
-        $("#container").fadeIn();
-        TweenMax.from("#container", .4, { scale: 0, ease: Sine.easeInOut });
-        TweenMax.to("#container", .4, { scale: 1, ease: Sine.easeInOut });
-    });
+﻿$(".login__btn--close").click(function () {
+    $("#forgotten-container").fadeOut();
+    document.getElementById("container").style.display = "block";
 });
 
-$(".login__btn--close").click(function () {
-    TweenMax.from("#container", .4, { scale: 1, ease: Sine.easeInOut });
-    TweenMax.to("#container", .4, { left: "0px", scale: 0, ease: Sine.easeInOut });
-    $("#container, #forgotten-container").fadeOut(800, function () {
-        $("#login__btn").fadeIn(800);
-    });
-});
 
 /* Forgotten Password */
 $('#forgotten').click(function () {
