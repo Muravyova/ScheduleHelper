18a6d0664da9a235e5498a41d8a764589781c189
VarHoro <varhoro@mail.ru>
2019-01-23 21:47:53 +0700

    более менее работает

--- Test2App/wwwroot/js/site.js
+++ Test2App/wwwroot/js/site.js
@@ -4,6 +4,7 @@ const days = document.querySelectorAll('.weekdays');
 
 NodeList.prototype.forEach = Array.prototype.forEach;
 
+//обработка hover
 function z(event) {
     var target = event.target;
     target.style.zIndex = parseInt(target.style.zIndex) + 100;
@@ -14,54 +15,15 @@ function zmin(event) {
     target.style.zIndex = parseInt(target.style.zIndex) - 100;
 }
 
-//function contains(a, obj) {
-//    var i = a.length;
-//    while (i--) {
-//        if (a[i] === obj) {
-//            return true;
-//        }
-//    }
-//    return false;
-//}
-
-    //day_obj.forEach(function (obj) {
-    //    var hour = null;
-    //    for (var i = 0; i < obj.childNodes.length; i++) {
-    //        if (obj.childNodes[i].className == "hidden time") {
-    //            hour = parseInt(obj.childNodes[i].innerHTML, 10);
-    //            if (same[hour] > 0) {
-
-    //                //obj.classList.add("hidden");
-    //                //if (check[time] == 0) {
-    //                //    check[time]++;
-    //                //    var div = document.createElement('div');
-    //                //    div.style.top = 170 + (time - 6) * 60 + "px";
-    //                //    div.classList.add("shedule-obj");
-    //                //    div.classList.add("special");
-    //                //    div.innerHTML = "Перекрытие";
-    //                //    day.appendChild(div);
-    //                //} 
-    //            }
-    //        }
-    //    }
-    //});
-    
-
-//var hid = document.querySelectorAll('.special');
-//hid.forEach(function (overlay) {
-//    overlay.addEventListener('click', () => {
-//        alert(1);
-//    })
-//});
-
+//если нажать на элемент, он выйдет на 10 пунктов вперед
 shobj.forEach(function (sho) {
     sho.addEventListener("click", () => {
-        sho.style.zIndex = parseInt(sho.style.zIndex) + 1;
+        sho.style.zIndex = parseInt(sho.style.zIndex) + 10;
     })
 })
 
+//обработка внешнего вида элементов
 shobj.forEach(function (sho) {
-
     var day = null;
     var time = null;
     var dur = null;
@@ -117,17 +79,12 @@ shobj.forEach(function (sho) {
     
 });
 
-
+//обработка перекрытий
 days.forEach(function (day) {
     var day_obj = day.childNodes;
     var same = []; //счетчик объестов с одинаковым временем(час)
     var qu = []; //очередь часов
     var objects = []; //очередь объектов, которые начинаются в один час
-
-    //for (var i = 0; i < day.childNodes.length; i++) {
-    //    check[i] = 0;
-    //}
-
     //находим все перекрывающиеся объекты
     day_obj.forEach(function (obj) {
         var hour = null;
@@ -143,7 +100,7 @@ days.forEach(function (day) {
             }
         }
     });
-
+    //собираем одинаковые элементы
     day_obj.forEach(function (obj) {
         var hour = null;
         for (var i = 0; i < obj.childNodes.length; i++) {
@@ -165,7 +122,8 @@ days.forEach(function (day) {
                 ind++;
             }
             if (ind > -1) {
-                var wobj = objects;//.slice(ind, ind + same[h]);//плучили массив с объектами одного времени
+                var wobj = objects;//плучили массив с объектами одного времени
+                console.log(wobj);
                 for (var d = 0; d < same[h]; d++) { objects.shift; }
                 for (var l = 0; l < wobj.length; l++) {//проходим через этот массив
                     var i = 0;
@@ -183,100 +141,74 @@ days.forEach(function (day) {
                         }
                         i++;
                     }
-                    
-                    var e = document.querySelectorAll('.hidden.serch');
-                    var blank = null;
-                    
-                    e.forEach(function (elem) {
-                        if (elem.innerHTML == time) {
-                            blank = { elem };
-                            
-                        }
-                    })
+
                     if (dur == 60) {
                         if (min == 0) {
                             if ((m0 > 0) && (m0 < 5)) {
-                                blank.elem.parentNode.style.zIndex = 120 + m0;
-                                blank.elem.parentNode.style.width = 150 - m0 * 10 + "px";
+                                wobj[l].style.zIndex = 120 + m0;
+                                wobj[l].style.width = 150 - m0 * 10 + "px";
                             } else {
-                                blank.elem.parentNode.style.zIndex = 120;
+                                wobj[l].style.zIndex = 120;
                             }
                             m0++;
-                            console.log(blank.elem.innerHTML);
-                            console.log(blank.elem.parentNode.style.zIndex);
                         } else if (min == 15) {
                             if ((m15 > 0) && (m15 < 5)) {
-                                blank.elem.parentNode.style.zIndex = 100 + m0;
-                                blank.elem.parentNode.style.width = 150 - m0 * 10 + "px";
+                                wobj[l].style.zIndex = 100 + m15;
+                                wobj[l].style.width = 150 - m15 * 10 + "px";
                             } else {
-                                blank.elem.parentNode.style.zIndex = 100;
+                                wobj[l].style.zIndex = 100;
                             }
                             m15++;
-                            console.log(blank.elem.innerHTML);
-                            console.log(blank.elem.parentNode.style.zIndex);
                         } else if (min == 30) {
                             if ((m30 > 0) && (m30 < 5)) {
-                                blank.elem.parentNode.style.zIndex = 80 + m0;
-                                blank.elem.parentNode.style.width = 150 - m0 * 10 + "px";
+                                wobj[l].style.zIndex = 80 + m30;
+                                wobj[l].style.width = 150 - m30 * 20 + "px";
                             } else {
-                                blank.elem.parentNode.style.zIndex = 80;
+                                wobj[l].style.zIndex = 80;
                             }
                             m30++;
-                            console.log(blank.elem.innerHTML);
-                            console.log(blank.elem.parentNode.style.zIndex);
                         } else if (min == 45) {
                             if ((m15 > 0) && (m45 < 5)) {
-                                blank.elem.parentNode.style.zIndex = 60 + m0;
-                                blank.elem.parentNode.style.width = 150 - m0 * 10 + "px";
+                                wobj[l].style.zIndex = 60 + m45;
+                                wobj[l].style.width = 150 - m45 * 20 + "px";
                             } else {
-                                blank.elem.parentNode.style.zIndex = 60;
+                                wobj[l].style.zIndex = 60;
                             }
                             m45++;
-                            console.log(blank.elem.innerHTML);
-                            console.log(blank.elem.parentNode.style.zIndex);
                         }
                     } else {
                         if (min == 0) {
                             if ((m0 > 0) && (m30 > 0)) {
-                                blank.elem.parentNode.style.top = parseInt(blank.parentNode.style.top, 10) +  5 + "px";
+                                wobj[l].style.top = parseInt(wobj[l].style.top, 10) +  5 + "px";
                             }
-                            blank.elem.parentNode.style.zIndex = 80 + m30 + m0d;
+                            wobj[l].style.zIndex = 80 + m30 + m0d;
                             if ((m0d > 0) && (m0d < 5)) {
-                                blank.elem.parentNode.style.width = 150 - m0d * 10 + "px";
+                                wobj[l].style.width = 150 - m0d * 20 + "px";
                             }  
                             m0d++;
-                            console.log(blank.elem.innerHTML);
-                            console.log(blank.elem.parentNode.style.zIndex);
                         } else if (min == 15) {
                             if ((m15 > 0) && (m45 > 0)) {
-                                blank.elem.parentNode.style.top = parseInt(blank.parentNode.style.top, 10) + 5 + "px";
+                                wobj[l].style.top = parseInt(wobj[l].style.top, 10) + 5 + "px";
                             }
-                            blank.elem.parentNode.style.zIndex = 60 + m45 + m15d;
+                            wobj[l].style.zIndex = 60 + m45 + m15d;
                             if ((m15d > 0) && (m15d < 5)) {
-                                blank.elem.parentNode.style.width = 150 - m15d * 10 + "px";
+                                wobj[l].style.width = 150 - m15d * 20 + "px";
                             }
                             m15d++;
-                            console.log(blank.elem.innerHTML);
-                            console.log(blank.elem.parentNode.style.zIndex);
                         } else if (min == 30) {
                             if ((m30d > 0) && (m30d < 5)) {
-                                blank.elem.parentNode.style.width = 150 - m30d * 10 + "px";
+                                wobj[l].style.width = 150 - m30d * 20 + "px";
                             }
-                            blank.elem.parentNode.style.zIndex = 40 + m30d;
+                            wobj[l].style.zIndex = 40 + m30d;
                             m30d++;
-                            console.log(blank.elem.innerHTML);
-                            console.log(blank.elem.parentNode.style.zIndex);
                         } else if (min == 45) {
                             if ((m45d > 0) && (m45d < 5)) {
-                                blank.elem.parentNode.style.width = 150 - m45d * 10 + "px";
+                                wobj[l].style.width = 150 - m45d * 20 + "px";
                             }
-                            blank.elem.parentNode.style.zIndex = 20 + m45d;
+                            wobj[l].style.zIndex = 20 + m45d;
                             m45d++;
-                            console.log(blank.elem.innerHTML);
-                            console.log(blank.elem.parentNode.style.zIndex);
                         }
                     }
-                    console.log(m0 + ' ' + m15 + ' ' + m30 + ' ' + m45 + ' ' + m0d + ' ' + m15d + ' ' + m30d + ' ' + m45d);
                 }
             }
         }
